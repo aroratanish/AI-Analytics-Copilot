9a29084a789db5b139d19b884827771ced5f65f6
Tanish Arora <tarora_b24@thapar.edu>
2026-08-12 23:50:35 +0530

    Day 2: Flask dataset upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    file = request.files["file"]
+    file_path = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        file.filename
+    )
+    file.save(file_path)
+    if file.filename.endswith(".csv"):
+        df = pd.read_csv(file_path)
+    elif file.filename.endswith(".xlsx"):
+        df = pd.read_excel(file_path)
+    else:
+        return "Unsupported file type"
+    return render_template(
+        "dashboard.html",
+        tables=[df.head().to_html(classes="data")],
+        filename=file.filename
+    )
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
